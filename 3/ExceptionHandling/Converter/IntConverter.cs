6570d77713ddb151c15088a77f6c86648399e7be
Tkacheva.KA <tkacheva@reksoft.ru>
2023-12-28 11:04:18 +0300

    3

---
+++ 3/ExceptionHandling/Converter/IntConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace Converter;
+
+public class IntConverter
+{
+    public static int ConvertStringToInt(string sentence)
+    {
+        if (string.IsNullOrEmpty(sentence))
+            throw new ArgumentNullException(sentence);
+
+        var isNegative = false;
+        if (sentence.FirstOrDefault().Equals('-'))
+        {
+            isNegative = true;
+            sentence = sentence.Remove(0, 1);
+        }
+        
+        if (sentence.Any(s => !char.IsNumber(s)))
+            throw new ArgumentException("String contains invalid symbols.");
+
+        var result = sentence.Aggregate(0, (current, symbol) => 
+            checked(current * 10 + (int)(char.GetNumericValue(symbol))));
+
+        return isNegative ? result * -1 : result;
+    }
+}
---
+++ 3/ExceptionHandling/FirstSymbolConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.Write("Input string: ");
+var s = Console.ReadLine();
+
+if(string.IsNullOrEmpty(s))
+    throw new ArgumentNullException(s);
+if(string.IsNullOrWhiteSpace(s))
+    throw new ArgumentException("Only white space is not allow.");
+
+Console.WriteLine(s.FirstOrDefault()); 
