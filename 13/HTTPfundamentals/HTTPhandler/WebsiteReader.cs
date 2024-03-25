9cf8a4897f4afa471ce923807ead6e4ec0663fe6
Tkacheva.KA <tkacheva@reksoft.ru>
2024-03-25 12:34:44 +0300

    13

---
+++ 13/HTTPfundamentals/HTTPhandler/WebsiteReader.cs
@@ -0,0 +1,63 @@
+﻿using HtmlAgilityPack;
+
+namespace HTTPhandler
+{
+    public class WebsiteReader
+    {
+        private readonly string _url;
+        private readonly HttpResponseMessage _response;
+
+        public WebsiteReader(string url)
+        {
+            _url = url;
+            _response = ConnectAsync().Result;
+        }
+
+        private async Task<HttpResponseMessage> ConnectAsync()
+        {
+            ArgumentNullException.ThrowIfNullOrEmpty(_url);
+
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(_url);
+            if (!response.IsSuccessStatusCode)
+                throw new Exception(response.StatusCode.ToString());
+
+            return response;
+        }
+
+        public async Task<IEnumerable<string>> Read()
+        {
+            var content = await _response.Content.ReadAsStringAsync();
+
+            var linkElements = new List<string>() { "link", "img", "a", };
+            var linkAttributes = new List<string>() { "src", "href" };
+
+            var document = new HtmlDocument();
+            document.LoadHtml(content);
+            var links = document.DocumentNode.Descendants()
+                .Where(x => linkElements.Any(le => le == x.Name))
+                .SelectMany(l => l.Attributes)
+                .Where(att => linkAttributes.Any(la => la == att.Name) && !att.Value.StartsWith("#"))
+                .Select(att => att.Value)
+                .Distinct();
+            
+            return links.Select(l => GenerateLink(l));
+        }
+
+        public async Task Save(string filePath)
+        {
+            var cgStream = await _response.Content.ReadAsStreamAsync();
+            var fileStream = File.Open(filePath, FileMode.Create);
+            await cgStream.CopyToAsync(fileStream);
+        }
+
+        private string GenerateLink(string link)
+        {
+            var http = "https";
+
+            return link.StartsWith("//") ? $"{http}:" + link :
+                   link.StartsWith($"/") ? $"{_url}{link}" :
+                   link.StartsWith(http) ? link : $"{_url}/{link}";
+        }
+    }
+}
---
+++ 13/HTTPfundamentals/TestConsole/Program.cs
@@ -0,0 +1,8 @@
+﻿using HTTPhandler;
+
+var sitePath = "https://ru.wikipedia.org/";
+var filePath = @"C:\Users\Tkacheva\Downloads\iii\new.html";
+
+var reader = new WebsiteReader(sitePath);
+await reader.Read();
+await reader.Save(filePath);
